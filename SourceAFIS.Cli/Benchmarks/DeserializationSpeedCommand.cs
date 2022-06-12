7224ef3f49f8b2a825abf236ad0ac538520ebdb4
Robert Važan <robert.vazan@tutanota.com>
2022-06-12 07:33:49 +0000

    Better blackholing of deserialization

--- SourceAFIS.Cli/Benchmarks/DeserializationSpeedCommand.cs
+++ SourceAFIS.Cli/Benchmarks/DeserializationSpeedCommand.cs
@@ -1,4 +1,5 @@
 // Part of SourceAFIS CLI for .NET: https://sourceafis.machinezoo.com/cli
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using SourceAFIS.Cli.Inputs;
@@ -14,13 +15,21 @@ namespace SourceAFIS.Cli.Benchmarks
         protected override Sampler<Fingerprint> Sampler() => new FingerprintSampler();
         class TimedDeserialization : TimedOperation<Fingerprint>
         {
+            readonly Random random = new Random();
             readonly Dictionary<Fingerprint, byte[]> serialized;
             byte[] input;
             FingerprintTemplate deserialized;
             public TimedDeserialization(Dictionary<Fingerprint, byte[]> serialized) => this.serialized = serialized;
             public override void Prepare(Fingerprint fp) => input = serialized[fp];
             public override void Execute() => deserialized = new FingerprintTemplate(input);
-            public override void Blackhole(Hasher hasher) => hasher.Add(deserialized.ToByteArray());
+            public override void Blackhole(Hasher hasher)
+            {
+                // We cannot just blackhole serialized template, because dead code elimination could skip populating transient fields.
+                // So we blackhole self-match score instead. That is however very expensive, so we do it only very rarely.
+                // Dead code elimination is nevertheless disabled in all cases, because compiler cannot predict the RNG.
+                if (random.Next(100_000) == 1)
+                    hasher.Add(new FingerprintMatcher(deserialized).Match(deserialized));
+            }
         }
         public override TimingData Measure()
         {
