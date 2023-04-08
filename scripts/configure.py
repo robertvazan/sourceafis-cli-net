e02859c671d32dfec492b2894212e12dbb2f2668
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 17:29:53 +0000

    Updated project configuration

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,35 +1,33 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import pathlib
-project_directory = lambda: pathlib.Path(__file__).parent.parent
-config_directory = lambda: project_directory().parent/'project-config'
-exec((config_directory()/'src'/'net.py').read_text())
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-root_namespace = lambda: 'SourceAFIS.Cli'
-pretty_name = lambda: 'SourceAFIS CLI for .NET'
-nuget_title = lambda: 'SourceAFIS CLI'
-repository_name = lambda: 'sourceafis-cli-net'
-subdomain = lambda: 'sourceafis'
-homepage = lambda: website() + 'cli'
-inception_year = lambda: 2021
-nuget_description = lambda: 'Command-line interface for SourceAFIS.'
-nuget_tags = lambda: 'fingerprint; biometrics; authentication; sourceafis'
-md_description = lambda: '''\
-    SourceAFIS CLI for .NET is a command-line interface to [SourceAFIS for .NET](https://sourceafis.machinezoo.com/net).
-    At the moment, it can benchmark algorithm accuracy, template footprint, and implementation speed.
-    It also includes tools that aid in development of SourceAFIS for .NET.
-    Read more on [SourceAFIS CLI](https://sourceafis.machinezoo.com/cli) page.
-'''
+class Project(scaffold.Net):
+    def script_path_text(self): return __file__
+    def root_namespace(self): return 'SourceAFIS.Cli'
+    def pretty_name(self): return 'SourceAFIS CLI for .NET'
+    def nuget_title(self): return 'SourceAFIS CLI'
+    def repository_name(self): return 'sourceafis-cli-net'
+    def inception_year(self): return 2021
+    def nuget_description(self): return 'Command-line interface for SourceAFIS.'
+    def nuget_tags(self): return 'fingerprint; biometrics; authentication; sourceafis'
+    def project_status(self): return self.stable_status()
+    def md_description(self): return '''\
+        SourceAFIS CLI for .NET is a command-line interface to [SourceAFIS for .NET](https://sourceafis.machinezoo.com/net).
+        At the moment, it can benchmark algorithm accuracy, template footprint, and implementation speed.
+        It also includes tools that aid in development of SourceAFIS for .NET.
+        Read more on [SourceAFIS CLI](https://sourceafis.machinezoo.com/cli) page.
+    '''
 
-def documentation_links():
-    yield 'SourceAFIS CLI', 'https://sourceafis.machinezoo.com/cli'
-    yield 'SourceAFIS for .NET', 'https://sourceafis.machinezoo.com/net'
-    yield 'SourceAFIS overview', 'https://sourceafis.machinezoo.com/'
+    def documentation_links(self):
+        yield 'SourceAFIS CLI', 'https://sourceafis.machinezoo.com/cli'
+        yield 'SourceAFIS for .NET', 'https://sourceafis.machinezoo.com/net'
+        yield 'SourceAFIS overview', 'https://sourceafis.machinezoo.com/'
 
-def dependencies():
-    use('SourceAFIS:3.14.0')
-    use('System.Net.Http:4.3.4')
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use('SourceAFIS:3.14.0')
+        yield self.use('System.Net.Http:4.3.4')
 
-generate()
+Project().generate()
